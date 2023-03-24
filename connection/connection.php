17c3ad5bd77d4266a564c752f018034b187e1b61
amrik131053 <amrik@gku.ac.in>
2023-03-24 09:55:30 +0530

    Upload practicle and workshop marks

--- connection/connection.php
+++ connection/connection.php
@@ -10,7 +10,7 @@ $conn91 = sqlsrv_connect( $serverName,$connectionInfo1);
 $connection_web_in_website= new mysqli('119.18.54.49:3306', 'guruk2cy_connect','Amrik@123','guruk2cy_website');
 
 
-
+ 
 // $servername1 = "localhost";
 // $username1 = "root";
 // $password1 = "";
