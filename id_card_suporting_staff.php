e03ae862cefcc9249f6c9f7e3b70c48600224a8b
amrik131053 <amrik@gku.ac.in>
2023-04-16 11:21:34 +0530

    outside mess

--- id_card_suporting_staff.php
+++ id_card_suporting_staff.php
@@ -6,87 +6,8 @@ include "connection/connection.php";
 <script src="plugins/webcam.js/webcam.js"> </script>
 <section class="content">
    <div class="container-fluid">
-      <div class="row">
-
-         <!-- left column -->
-          
-         <!-- Button trigger modal -->
-         <div class="col-lg-12 col-md-12 col-sm-12">
-            <div class="card card-info">
-               <div class="card ">
-                  
-                <div class="row">
-                            <div class="card-body  ">
-                  
-                    <a class="btn"  id="btn6"  style="background-color:#223260; color: white; border: 1px solid;" onclick="ID_card();bg(this.id);"> ID Card </a>
-                  <!-- </div> -->
-              </div>
-                <!-- </div> -->
-              </div>
-            </div>
-
-               
-    
-                  <div class="card-body">
-                    <div class="container-fluid">
- 
-</div>
-                     <div class="form-group card-body table-responsive  row ">
 
 
-
-<table class="table table-striped" id="example">
-         <thead>
-          <tr> 
-         <th><input type="checkbox" name=""></th>  
-         <th>Image</th>            
-          <th>Name</th>
-         <th>Father Name</th>
-          <th>ID</th>
-          <th>Department</th>
-          <th>Designation</th>
-          <th>Mobile Number</th>  
-          <th>Address</th> 
-          <th>Status</th>      
-         
-         </tr>
-                   </thead>
-    <tbody id="d_card_record">
-<?php
-$sql="SELECT * FROM Staff where Designation='Peon' or Designation='Swiper' or Designation='Gardener' or Designation='Sweeper' ";
-$result = sqlsrv_query($conntest,$sql); 
-while($row=sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC) )
-{
-$IDNo=$row['IDNo'];
-$external_link = 'http://10.0.10.11:86/images/Staff/'.$IDNo.'.jpg';
-if (@getimagesize($external_link)) {
-?>
- <tr>
-    <td><input type="checkbox" name="" class="sel" value="<?=$row['IDNo'];?>" ></td>
-    <td> <?php echo '<img src="http://10.0.10.11:86/images/Staff/'.$IDNo.'.jpg" height="50" width="50" class="img-thumnail" style="border-radius:50%">';?>   
-    </td>
-     <td><?=$row['Name'];?></td>
-      <td><?=$row['FatherName'];?></td>
-       <td><?=$row['IDNo'];?></td>
-        <td><?=$row['Department'];?></td>
-         <td><?=$row['Designation'];?></td>
-          <td><?=$row['MobileNo'];?></td>
-          <td><?=$row['PermanentAddress']; 
-               ?>
-            </td>
-            <td><?php
-            $IDNo=$row['IDNo'];
-               $sql1="SELECT * FROM Suporting_staff where IDNo='$IDNo'";
-$result1 = mysqli_query($conn,$sql1); 
-if($row1=mysqli_fetch_array($result1) )
-{
-          if ($row1['Status']==1)
-           {?><P style="color:red;">Printed</P>
-         <div class="col-lg-12 col-md-12 col-sm-12" >
-
-            
-                  
-              
             <div class="card card-info collapsed-card" >
                <div class="card-header">
                   <h3 class="card-title">Supporting Staff</h3>
@@ -100,11 +21,12 @@ if($row1=mysqli_fetch_array($result1) )
                   </button>
                   </div>
                </div>
-              
+      
                <!-- /.card-header -->
                <div class="card-body " >
                    <form id="submitGateEntry" action="action.php" method="post" enctype="multipart/form-data">
                      <input type="hidden" name="code" value='276'>
+
                   <div class="row">
                  
                    
