9942ccf7c8925008cc2ff881c222b1791959e1fa
amrik131053 <amrik@gku.ac.in>
2023-05-15 10:09:16 +0530

    merge gurpreet mastercourse and task

--- connection/connection.php
+++ connection/connection.php
@@ -1,7 +1,7 @@
 <?php
  
 $serverName = "103.18.70.79"; //serverName\instanceName
-$connectionInfo = array( "Database"=>"DBgurukashitest", "UID"=>"sa", "PWD"=>"b2y3rt78374&*#&$");
+$connectionInfo = array( "Database"=>"DBgurukashi", "UID"=>"sa", "PWD"=>"b2y3rt78374&*#&$");
 $conntest = sqlsrv_connect( $serverName,$connectionInfo);
 
 $connectionInfo1 = array( "Database"=>"App91", "UID"=>"sa", "PWD"=>"b2y3rt78374&*#&$");
@@ -10,42 +10,19 @@ $conn91 = sqlsrv_connect( $serverName,$connectionInfo1);
 $connection_web_in_website= new mysqli('119.18.54.49:3306', 'guruk2cy_connect','Amrik@123','guruk2cy_website');
 
 
- 
-<<<<<<< HEAD
-// 		$servername1 = "localhost";
-// $username1 = "root";
-// $password1 = "";
-// $dbname1 = "lims";
-
- 
-		$servername1 = "10.0.8.10";
-$username1 = "as";
-$password1 = "Bathinda@123";
-$dbname1 = "lims";
-
-
-
-
-
-=======
 $servername1 = "localhost";
 $username1 = "root";
 $password1 = "";
 $dbname1 = "lims";
 
 
->>>>>>> gurpreet
+
 // $servername1 = "localhost";
 // $username1 = "bhagi";
 // $password1 = "@Sarbjot@98157";
 // $dbname1 = "lims";
 
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> gurpreet
 $conn = new mysqli($servername1, $username1, $password1, $dbname1);
 
 
