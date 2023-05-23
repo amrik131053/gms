0c2482488027622f817f74b98d434d7cec873708
amrik131053 <amrik@gku.ac.in>
2023-05-23 16:55:30 +0530

    movement admin

--- interview_marks.php
+++ interview_marks.php
@@ -17,75 +17,16 @@
                </div>
               
                   <div class="card-body">
-                     <div class="card-body table-responsive">
- <table class="table">
-  
-         <th>Sr No</th> <th>Reg ID</th><th>Name</th><th>Department/Subject</th><th>ResearchApptitude</th><th>SubjectContent</th><th>Communication</th><th>TeachingSkills</th><th>Action</th>
-               <?php 
-
-               $sr=1; 
-
-               $panel="SELECT *,basic_detail.id as myid FROM  panel_members INNER JOIN panel ON panel.id =panel_members.panel_id 
-               inner join basic_detail on panel.PanelSubject=basic_detail.department inner join recruitment as rec on rec.r_id=panel.recruitment 
-
-                WHERE panel_members.emp_id='$EmployeeID' AND panel.interviewDate='$date'  ANd rec.r_status='Active' ANd Eligibility='1' ";
-
-                  $panel_run=mysqli_query($conn_recruitment,$panel);
-                                    while ($p_row=mysqli_fetch_array($panel_run)) 
-                                    {
-
-                                       ?>
-                                       <tr>
-         <td><?php echo $sr;?> </td>
-         <td>  <b><?php echo  $muids=$p_row['myid'];?></b></td> 
-                <td>  <b><?php echo $p_row['candidate_name'];?><?php echo $p_row['recruitment'];?></b></td> 
-                  <td>  <b><?php echo $p_row['department'];?></b></td>
-<?php 
- $panel_e="SELECT * FROM  evaluation where recruitment_id='$muids'";
-
-                  $panel_eva=mysqli_query($conn_recruitment,$panel_e);
-
-                  if(mysqli_num_rows($panel_eva)>0)
-                  {
-                    while ($pe_row=mysqli_fetch_array($panel_eva)) 
-                                    {
-
-                                       ?>
-                  
-
-                  <td> <input type="number" class="form-control" style="width: 80px;" value="<?php echo $pe_row['ResearchApptitude'];?>"></td> 
-                  <td> <input type="number" class="form-control" style="width: 80px;" value="<?php echo $pe_row['SubjectContent'];?>"></td> 
-                  <td> <input type="number" class="form-control" style="width: 80px;" value="<?php echo $pe_row['Communication'];?>"></td> 
-                  <td> <input type="number" class="form-control" style="width: 80px;" value="<?php echo $pe_row['TeachingSkills'];?>"></td>
-                  <td><button class="btn btn-warning btn-xs"> Update</button> <button class="btn btn-danger btn-xs"> Lock</button></td>
-
-
-                                <?php 
-                             }
-                          }
-                          else
-                          {?>
-<td> <input type="number" class="form-control" style="width: 80px;" ></td> 
-                  <td> <input type="number" class="form-control" style="width: 80px;" ></td> 
-                  <td> <input type="number" class="form-control" style="width: 80px;" ></td> 
-                  <td> <input type="number" class="form-control" style="width: 80px;" ></td> 
-<td><button class="btn btn-info btn-xs">Update</button> </td> 
-                        <?php  }
-
-
-
-                                $sr ++;   }
-                                    ?>
-                                    </tr>
-     </table>
-                  </div>
+                     
+<div id="candidate_data_show"></div>
+                 
               
      
         
   </div>
          <div class="card-footer">
 <center>
-             <input type="button" class="btn btn-primary" name="create" value="Final Submit" onclick="id_card_data_submit();"  >
+             <!-- <input type="button" class="btn btn-primary" name="create" value="Final Submit" onclick="id_card_data_submit();"  > -->
            </center>
          </div>
         
@@ -105,3 +46,174 @@
    include "footer.php"; 
    
     ?>
+    <script type="text/javascript">
+      $(window).on('load', function() 
+          {
+           loaddata();
+    
+           })
+
+
+ function correctmarks(id)
+{
+
+var code=3;
+ var research=parseFloat(document.getElementById(id+'_research').value);
+ var ts=parseFloat(document.getElementById(id+'_ts').value);
+ var sc=parseFloat(document.getElementById(id+'_sc').value);
+ var comm=parseFloat(document.getElementById(id+'_comm').value);
+var spinner=document.getElementById("ajax-loader");
+if(research<=10 && research>=0 && ts<=15 && ts>=0 && sc<=15 && sc>=0 &&comm<=10 && comm>=0 )
+{
+     spinner.style.display='block';
+  $.ajax({
+        url: "web_action.php",
+        type: "POST",
+        data: {code:code,research:research,ts:ts,sc:sc,comm:comm,id:id},
+        success: function(data) {
+      
+
+         spinner.style.display='none';
+          loaddata();
+          
+        }
+      });
+     }
+     else{
+      alert("Marks are more than max marks")
+
+     }  
+       
+} 
+
+function lock(id)
+{
+
+var code=4;
+
+ var research=parseFloat(document.getElementById(id+'_research').value);
+ var ts=parseFloat(document.getElementById(id+'_ts').value);
+ var sc=parseFloat(document.getElementById(id+'_sc').value);
+ var comm=parseFloat(document.getElementById(id+'_comm').value);
+var spinner=document.getElementById("ajax-loader");
+if(research<=10 && research>=0 && ts<=15 && ts>=0 && sc<=15 && sc>=0 &&comm<=10 && comm>=0 )
+{
+ 
+     spinner.style.display='block';
+  $.ajax({
+        url: "web_action.php",
+        type: "POST",
+        data: {code:code,research:research,ts:ts,sc:sc,comm:comm,id:id},
+        success: function(data) {
+      
+
+         spinner.style.display='none';
+          loaddata();
+          
+        }
+      });
+    }
+     else{
+      alert("Marks are more than max marks")
+
+     }
+
+
+
+}
+
+
+
+
+ function UpdateMarks(id)
+{
+
+var code=1;
+ var panel_id=document.getElementById('panel_id').value;
+ var research=parseFloat(document.getElementById(id+'_research').value);
+ var ts=parseFloat(document.getElementById(id+'_ts').value);
+ var sc=parseFloat(document.getElementById(id+'_sc').value);
+ var comm=parseFloat(document.getElementById(id+'_comm').value);
+var spinner=document.getElementById("ajax-loader");
+if(research<=10 && research>=0 && ts<=15 && ts>=0 && sc<=15 && sc>=0 &&comm<=10 && comm>=0 )
+{
+ 
+     spinner.style.display='block';
+  $.ajax({
+        url: "web_action.php",
+        type: "POST",
+        data: {code:code,research:research,ts:ts,sc:sc,comm:comm,id:id,panel_id:panel_id},
+        success: function(data) {
+      
+
+         spinner.style.display='none';
+          loaddata();
+          
+        }
+      });
+    }
+     else{
+      alert("Marks are more than max marks")
+
+     }     
+      
+}
+
+function Absent(id)
+{
+      var r = confirm("realy want to mark abset");  
+    if (r == true) {  
+var code=5;
+var panel_id=document.getElementById('panel_id').value;
+ var research=parseFloat(document.getElementById(id+'_research').value);
+ var ts=parseFloat(document.getElementById(id+'_ts').value);
+ var sc=parseFloat(document.getElementById(id+'_sc').value);
+ var comm=parseFloat(document.getElementById(id+'_comm').value);
+var spinner=document.getElementById("ajax-loader");
+if(research<=10 && research>=0 && ts<=15 && ts>=0 && sc<=15 && sc>=0 &&comm<=10 && comm>=0 )
+{
+ 
+     spinner.style.display='block';
+  $.ajax({
+        url: "web_action.php",
+        type: "POST",
+        data: {code:code,research:research,ts:ts,sc:sc,comm:comm,id:id,panel_id:panel_id},
+        success: function(data) {
+      
+
+         spinner.style.display='none';
+          loaddata();
+          
+        }
+      });
+    }
+     else{
+      alert("Marks are more than max marks")
+
+     }     
+      
+   
+
+}
+}
+
+function   loaddata()
+{
+   var code=2;
+   var spinner=document.getElementById("ajax-loader");
+     spinner.style.display='block';
+  $.ajax({
+        url: "web_action.php",
+        type: "POST",
+        data: {code:code},
+        success: function(data) {
+      
+
+         spinner.style.display='none';
+          $("#candidate_data_show").html(data);
+        }
+      });
+       
+
+}
+ </script>
