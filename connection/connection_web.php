466c22599ea339c7ffc4367f69e7a3d1489f778a
amrik131053 <amrik@gku.ac.in>
2023-05-24 12:47:37 +0530

    movement admin

--- connection/connection_web.php
+++ connection/connection_web.php
@@ -3,6 +3,7 @@
 
 
 $connection_web_in_website= new mysqli('119.18.54.49:3306', 'guruk2cy_connect','Amrik@123','guruk2cy_website');
+
 $conn_recruitment= new mysqli('119.18.54.49:3306', 'guruk2cy_connect','Amrik@123','guruk2cy_recruitment');
 
 
