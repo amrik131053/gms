4a1dfc037b572239d38d25d3e378aea8dc593c63
amrik131053 <amrik@gku.ac.in>
2023-05-26 14:14:11 +0530

    manage student

--- manage_student.php
+++ manage_student.php
@@ -211,28 +211,15 @@ input[type=radio]:checked + label {
 </div>
 <script type="text/javascript">
  
+
 function copyToClipboard(element) {
   var $temp = $("<input>");
   $("body").append($temp);
   $temp.val($(element).text()).select();
   document.execCommand("copy");
   $temp.remove();
+  SuccessToast('Copied');
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
