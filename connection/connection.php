19e390b112ea63f88bfbcac45604392a7bf4926c
amrik131053 <amrik@gku.ac.in>
2023-07-14 15:08:42 +0530

    degree

--- connection/connection.php
+++ connection/connection.php
@@ -7,18 +7,9 @@ $conntest = sqlsrv_connect($serverName,$connectionInfo);
 $connectionInfo1 = array( "Database"=>"App91", "UID"=>"sa", "PWD"=>"b2y3rt78374&*#&$");
 $conn91 = sqlsrv_connect( $serverName,$connectionInfo1);
 
-// $servername1 = "localhost";
-// $username1 = "root";
-// $password1 = "";
-
-
-
-$servername1 = "10.0.8.10";
-$username1 = "as";
-$password1 = "Bathinda@123";
-// $servername1 = "localhost";
-// $username1 = "root";
-// $password1 = "";
+$servername1 = "localhost";
+$username1 = "bhagi";
+$password1 = "@Sarbjot@98157";
 
 
  $dbname1 = "lims";
