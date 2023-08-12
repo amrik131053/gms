90b3853d5bac86ceb51bea51276fa0433a2bbe27
Amrik Singh <creativecodes3@gmail.com>
2023-08-12 12:31:03 +0530

    hii

--- print_offer_letter.php
+++ print_offer_letter.php
@@ -256,8 +256,8 @@ $Y=$pdf->GETY();
 
 
 
-$pdf-> Image('dist/img/sign-offer.png',$X-18, $Y+5,24,20.5);
-$pdf-> Image('dist/img/sign.png',$X-10, $Y-15,30,26.5);
+$pdf-> Image('dist/img/sign-offer.png',$X-12, $Y+5,24,20.5);
+$pdf-> Image('dist/img/sign.png',$X-10, $Y-12,30,26.5);
 
 // $pdf-> Image('dist/img/sign-offer.png',150,230,24,20.5);
 
