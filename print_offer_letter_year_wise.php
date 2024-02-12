b50249667567fc7d982ec0b567f99c015e76897a
Amrik Singh <creativecodes3@gmail.com>
2024-02-12 11:55:29 +0530

    admisison

--- print_offer_letter_year_wise.php
+++ print_offer_letter_year_wise.php
@@ -551,13 +551,12 @@ $Y=$pdf->GETY();
 
 if($typeFromUII==1)
 {
-    $pdf-> Image('dist/img/sign-offer.png',160,170,24,20.5);
-
- $pdf-> Image('dist/img/sign.png',160, 150,30,26.5);
 }
 
 else{
- 
+ $pdf-> Image('dist/img/sign-offer.png',160,170,24,20.5);
+
+ $pdf-> Image('dist/img/sign.png',160, 150,30,26.5);
 }
 
 
