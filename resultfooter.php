34b60de927ff75fc2af40f2e6c42d2efe80e9f48
Amrik Singh <creativecodes3@gmail.com>
2024-02-14 11:55:07 +0530

    offer letter

--- resultfooter.php
+++ resultfooter.php
@@ -1,16 +1,16 @@
-<?php $exportstudy.="<tr border='none'><th colspan='".$subCount."' ><b style='font-size:15px;'>NC: Semester not completed.       F: The student has to reappear in internal and/or external examination.      DT: Detained,      NA: Not Applicable,    RL: Result Late,   AB: Absent UMC: Unfair Means Case, RLE: Result Late Due to Eligibilty, RLA: Result Late Due to Award List, * Already Has been Passed, Revaluation should be submitted within 15 Days after the date of declaration of the result.</b></th>         
+<?php $exportstudy.="<tr border='none'><th colspan='".$subCount."' ><Br><b style='font-size:15px;'>NC: Semester not completed.       F: The student has to reappear in internal and/or external examination.      DT: Detained,      NA: Not Applicable,    RL: Result Late,   AB: Absent UMC: Unfair Means Case, RLE: Result Late Due to Eligibilty, RLA: Result Late Due to Award List, * Already Has been Passed, Revaluation should be submitted within 15 Days after the date of declaration of the result.</b><Br><br></th>         
     </tr>";
  
 $fcopspan=(INT)$subCount/3;
 
-    $exportstudy.="<tr style='min-height:100px'>
+    $exportstudy.="<tr style='min-height:100px;font-size:18px'>
 
 
 
-    <th colspan='".$fcopspan."' ><br><Br><br><Br><b style='font-size:15px;'> Prepared By ________________________________&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>
+    <th colspan='".$subCount."' ><br><Br><br><b style='font-size:22px;'> Prepared By ________________________________&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
-        <th colspan='".$fcopspan."' > Checked By   
-        ________________________________&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th> <th colspan='".$fcopspan."' >Controller of Examinations ________________________________&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  </b><br><Br><br><Br></th>         
+       Checked By   
+        ________________________________&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Controller of Examinations ________________________________&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  </b><Br></th>         
     </tr>";
    
 ?>
