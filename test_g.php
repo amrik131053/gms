f28b7e265ac92bcfe3720139427d396fbdf41800
Gurpreet <gurpreetjalalana11@gmail.com>
2024-04-16 14:01:42 +0530

    re

--- test_g.php
+++ test_g.php
@@ -1,14 +1,14 @@
 <?php 
- $allSessions = array();
-$sessionNames = scandir(session_save_path());
-foreach($sessionNames as $sessionName) {
-    $sessionName = str_replace("sess_","",$sessionName);
-    if(strpos($sessionName,".") === false) {
-        session_id($sessionName);
-        session_start();
-        $allSessions[$sessionName] = $_SESSION;
-        session_abort();
-    }
-}
-print_r($allSessions);
+//  $allSessions = array();
+// $sessionNames = scandir(session_save_path());
+// foreach($sessionNames as $sessionName) {
+//     $sessionName = str_replace("sess_","",$sessionName);
+//     if(strpos($sessionName,".") === false) {
+//         session_id($sessionName);
+//         session_start();
+//         $allSessions[$sessionName] = $_SESSION;
+//         session_abort();
+//     }
+// }
+// print_r($allSessions);
 ?>
