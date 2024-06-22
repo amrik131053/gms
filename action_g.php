5a23113ecd7737b80c7d8a4097bced69578ca2ba
Amrik Singh <creativecodes3@gmail.com>
2024-06-22 11:14:44 +0530

    department

--- action_g.php
+++ action_g.php
@@ -10085,7 +10085,7 @@ $state_id=$_POST['state_id'];
 
  //$sql = "SELECT  id,name FROM cities WHERE state_id='$state_id'  order by name ASC";
 
-  $sql = "SELECT  cities.id AS id,name FROM cities INNER JOIN offer_admission_count ON cities.id=offer_admission_count.District   WHERE state_id='5' AND offer_admission_count.count>0 order by name ASC";
+  $sql = "SELECT  cities.id AS id,name FROM cities INNER JOIN offer_admission_count ON cities.id=offer_admission_count.District   WHERE state_id='$state_id' AND offer_admission_count.count>0 order by name ASC";
 
 $stmt = mysqli_query($conn,$sql); 
 ?>
@@ -10361,7 +10361,7 @@ $fileName=$image_name.'.PNG';
          // echo json_encode($pagedData);
       
              echo json_encode($pagedData);
-         //print_r($pagedData);
+         // print_r($pagedData);
       }
       else
       {
