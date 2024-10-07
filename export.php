2eb59b2c28664141f9fd155943a11f00960ed40a
Amrik Singh <creativecodes3@gmail.com>
2024-10-07 14:19:18 +0530

    convocation

--- export.php
+++ export.php
@@ -2863,7 +2863,7 @@ elseif($exportCode=='28')
       $Organisation = $row['course'];
       $IdNo = $row['Class_rollno'];
       $batch=$row['batch'];
-      $purpose=$row['purpose'];
+      $purpose=$row['remarks'];
       $remarks=$row['remarks'];
 
       $Created_date=$row['Created_date'];
