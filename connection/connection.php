faa5fc5b6fceb5b1a22a370b93c284e0398b08ff
Amrik Singh <113105386+amrik131053@users.noreply.github.com>
2025-01-31 10:29:07 +0530

    degree print

--- connection/connection.php
+++ connection/connection.php
@@ -5,8 +5,8 @@
       $todaydate=date('Y-m-d');
    $day=date('l'); 
    
-//$serverName = "10.0.10.11"; //serverName\instanceName
-$serverName = "117.250.20.109";
+$serverName = "10.0.10.11"; //serverName\instanceName
+//$serverName = "117.250.20.109";
 $connectionInfo = array( "Database"=>"DBgurukashi", "UID"=>"sa", "PWD"=>"b2y3rt78374&*#&$");
 
 
@@ -15,8 +15,8 @@ $connectionInfo1 = array( "Database"=>"App91", "UID"=>"sa", "PWD"=>"b2y3rt78374&
 $conn91 = sqlsrv_connect( $serverName,$connectionInfo1);
 
 
-//$servername1 = "10.0.8.10";
-$servername1 = "117.250.20.111";
+$servername1 = "10.0.8.10";
+//$servername1 = "117.250.20.111";
 
 $username1 = "as";
 $password1 = "Bathinda@123";
