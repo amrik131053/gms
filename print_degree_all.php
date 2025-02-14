e015580e3e9ee8f74a1536a19ab3c80cba664842
Amrik Singh <creativecodes3@gmail.com>
2025-02-14 12:32:46 +0530

    degree

--- print_degree_all.php
+++ print_degree_all.php
@@ -1366,7 +1366,17 @@ else{
             
                <body style="margin:0px; background-image: url('dgree_format1.jpg');background-size: 297mm 210mm; background-repeat: no-repeat; ">
                   <span class="notranslate">
+                      if($RegistrationNo!='')
+                        {
+                         ?>
                      <div style="height: 50px;"></div>
+                  <?php }
+                  else
+                  {?>
+<div style="height:60px;"></div>
+              <?php    } 
+                     // code...
+                  ?>
                      <!--60-->
                      <div class="row">
                   <?php  
