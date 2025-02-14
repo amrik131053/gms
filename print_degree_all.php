5f0cfdd6f2302bee14182c65299734c9010d869a
Amrik Singh <creativecodes3@gmail.com>
2025-02-14 12:38:07 +0530

    degree

--- print_degree_all.php
+++ print_degree_all.php
@@ -1373,7 +1373,7 @@ else{
                   <?php }
                   else
                   {?>
-<div style="height:60px;"></div>
+<div style="height:70px;"></div>
               <?php    } 
                      // code...
                   ?>
