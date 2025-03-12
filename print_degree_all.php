d21acdd530694888b9b853422d7175a96ef38683
Amrik Singh <creativecodes3@gmail.com>
2025-03-12 16:03:50 +0530

    data therpry update and degree

--- print_degree_all.php
+++ print_degree_all.php
@@ -480,7 +480,7 @@ $ms1="<strike>Mr.</strike>";    // code...
          
             <body style="margin:0px; background-image: url('dgree_format1.jpg');background-size: 297mm 210mm; background-repeat: no-repeat; ">
        <span class="notranslate">
-          <div style="height: 60px;"></div>
+          <div style="height: 50px;"></div>
           <div class="row">
              <!-- // space 74 -->
     <?php   if($RegistrationNo!='')
