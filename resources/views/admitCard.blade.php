101e9f610a1f62df148d206f2b2b77a046212b9f
Gurpreet <gurpreetjalalana11@gmail.com>
2025-04-17 09:45:26 +0530

    re

--- resources/views/admitCard.blade.php
+++ resources/views/admitCard.blade.php
@@ -83,7 +83,7 @@
                                     @endforeach
                                     @else
                                     <tr>
-                                        <td colspan="5" class="text-center text-danger">Admit Card Not Generete yet. please Check Prevoius Exam Form Status</td>
+                                        <td colspan="5" class="text-center text-danger">Admit Card not generated yet. Please check the previous exam form status.</td>
                                     </tr>
                                     @endif
 
