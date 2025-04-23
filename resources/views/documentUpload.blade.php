55e116ca5f58f61e56cdc0d8adb01f91aceb9411
Gurpreet <gurpreetjalalana11@gmail.com>
2025-04-23 13:39:59 +0530

    some changes

--- resources/views/documentUpload.blade.php
+++ resources/views/documentUpload.blade.php
@@ -23,7 +23,7 @@
             @endif
                         <div id="table-default" class="table-responsive">
                             <div class="table-responsive">
-                                <table class="table table-vcenter card-table">
+                                <table class="table table-vcenter card-table table-bordered">
                                     <thead>
                                         <tr>
                                             <th>SrNo</th>
@@ -66,6 +66,7 @@
                                                 @endif
                                             </td>
                                             <td>
+                                            <small style="color: green">*Document Size must be less than 1MB./jpg/jpeg/png/pdf</small>
                                                 @if ($showDocumentLitst['Action']==0 || $showDocumentLitst['Action']==2)
                                                 <form action="{{ route('documentupload') }}" method="POST"
                                                     enctype="multipart/form-data"
@@ -77,7 +78,7 @@
                                                         value="{{ $showDocumentLitst['IDNo'] }}">
                                                     <input type="file" class="form-control form-control-sm me-2"
                                                         name="document" style="max-width: 250px;">
-                                                    <button type="submit" class="btn btn-success btn-sm">Upload</button>
+                                                    <button type="submit" class="btn btn-success btn-sm"><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-upload"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" /><path d="M7 9l5 -5l5 5" /><path d="M12 4l0 12" /></svg>Upload</button>
                                                 </form>
                                                 @elseif($showDocumentLitst['Action']==1)
                                                 <b class="text-success">Documents Verified</b>
