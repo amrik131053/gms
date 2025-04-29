bf2d5127aded1a105f8c20a3d91f5f98ca955223
Amrik Singh <creativecodes3@gmail.com>
2025-04-29 15:32:59 +0530

    degree

--- print_offer_letter.php
+++ print_offer_letter.php
@@ -613,14 +613,14 @@ $Y=$pdf->GETY();
 
 $pdf->SetXY($X, $Y);
 
-$pdf->Cell(190, 4,'Bank Name         :   Indian Bank', 0, 'L');
+$pdf->Cell(190, 4,'Bank Name          :   Indian Bank', 0, 'L');
 $pdf->SetXY($X, $Y+5);
 $pdf->Cell(190, 4,'AccountNo          :   6058205486', 0, 'L');
 
 $pdf->SetXY($X, $Y+10);
-$pdf->Cell(190, 4,'IFSC Code          :    IDIB000F009', 0, 'L');
+$pdf->Cell(190, 4,'IFSC Code           :    IDIB000F009', 0, 'L');
 $pdf->SetXY($X, $Y+15);
-$pdf->Cell(190, 4,'Branch               :   Fatehgarh Naubad,Talwandi Sabo ', 0, 'L');
+$pdf->Cell(190, 4,'Branch                :   Fatehgarh Naubad,Talwandi Sabo ', 0, 'L');
 $pdf->SetXY($X, $Y+20);
 $pdf->Cell(190, 4,'Account Name    :   Guru Kashi University', 0, 'L');
 $X=$pdf->GETX();
