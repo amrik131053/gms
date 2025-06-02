e0e97b5cb1d9ed7c9dee1e4d9a4e534caed3e3cc
Gurpreet <gurpreetjalalana11@gmail.com>
2025-06-02 15:47:46 +0530

    pre register validation

--- action_g.php
+++ action_g.php
@@ -33476,7 +33476,7 @@ WHERE users.registration_number = '$registrationNumber'
 
     if ($degree_row = mysqli_fetch_array($degree_run)) {
         $has_data = true;
-        if ($degree_row['correct_answers'] > 2) { 
+        if ($degree_row['correct_answers'] > 1) { 
             $quiz_passed = true;
         }
         $days_over = $degree_row['days_over'] ?? 0;  
