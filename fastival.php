97a7dc7267c0512ed795fd4acb0cb31dd7141206
Gurpreet <gurpreetjalalana11@gmail.com>
2025-08-18 09:42:32 +0530

    logo set

--- fastival.php
+++ fastival.php
@@ -1,7 +1,7 @@
 <?php
 include "connection/connection.php";
 $Today=date('Y-m-d');
- $check_fastival="SELECT * FROM fastival_images Where  start_date_<= '$Today' and  end_date >= '$Today' and logo='1'";
+  $check_fastival="SELECT * FROM fastival_images Where  start_date_<= '$Today' and  end_date >= '$Today' and logo='1'";
 $check_fastival_run=mysqli_query($conn,$check_fastival);
 $count=mysqli_num_rows($check_fastival_run);
 if($count>0)
@@ -30,7 +30,7 @@ else{
 else
 {
     ?> 
-<img src="http://gurukashiuniversity.co.in/data-server/fastival/logo-main.png" class="brand_logo" alt="Logo" >
+<img src="dist/img/logo-main.png" class="brand_logo" alt="Logo" >
 <?php 
 
 }
